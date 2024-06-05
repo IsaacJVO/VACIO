fc057cdfd36d1bda444dd38285e10fcfc450b329
IsaacJVO <164194467+IsaacJVO@users.noreply.github.com>
2024-06-04 21:47:57 -0400

    Add files via upload

---
+++ Source/GALAGA_USFX/GALAGA_USFXGameMode.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GALAGA_USFXGameMode.generated.h"
+
+
+UCLASS(MinimalAPI)
+class AGALAGA_USFXGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGALAGA_USFXGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Facade Muros ")
+	class AFacadeMuros* Facade;
+	//**************************BUILDER*******************************
+
+	class Adirector* Director;
+	class AConstructorEscenario* BuilderEscenario;//constrrand
+};
+
+
+
---
+++ Source/GALAGA_USFX/director.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "director.h"
+#include "builderscenario.h"
+#include "escenario.h"
+
+// Sets default values
+Adirector::Adirector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Adirector::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Adirector::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void Adirector::ConstruirBloques()
+{
+	if (BuilderEscenario) //interface
+	{
+		BuilderEscenario->GetBlock1();
+		BuilderEscenario->GetBlock2();
+		BuilderEscenario->GetBlock3();
+	}
+}
+
+void Adirector::SetBuilder(AActor* Builder)
+{
+	BuilderEscenario = Cast<Ibuilderscenario>(Builder); // casteo noms
+}
+
+Aescenario* Adirector::GetEscenario()
+{
+	if (BuilderEscenario)
+	{
+		return BuilderEscenario->GetEscenario(); 
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+
+
